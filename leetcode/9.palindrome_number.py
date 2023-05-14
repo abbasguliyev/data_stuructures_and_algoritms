3399d0745392237bd08f215372cc6a9e8a33c544
abbasguliyev <abbasquliyev111@gmail.com>
2023-05-14 17:25:18 +0400

    palindrome

---
+++ leetcode/9.palindrome_number.py
@@ -0,0 +1,24 @@
+# First way
+# class Solution:
+#     def isPalindrome(self, x: int) -> bool:
+#         if x > 0:
+#             reverse_list = list()
+#             while x > 0:
+#                 residual = x % 10
+#                 reverse_list.append(residual)
+#                 x = x // 10
+#             num_list = reverse_list[::-1]
+#             return num_list == reverse_list
+#         elif x == 0:
+#             return True
+#         else:
+#             return False
+
+# Second Way
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        x = str(x)
+        return x == x[::-1]
+
+r = Solution().isPalindrome(x=121)
+print(f"{r=}")
